2cf2107121158ac1265f140b427a4470e4ef8043
Ethan Barton <barton.ethan@gmail>
2020-04-18 12:30:24 -0500

    test db local

--- app.js
+++ app.js
@@ -28,15 +28,15 @@ mongoose.set("useNewUrlParser", true);
 mongoose.set("useCreateIndex", true);
 mongoose.set("useFindAndModify", false);
 mongoose.set("useUnifiedTopology", true);
-// mongoose.connect("mongodb://localhost/yelp_camp_v22");
-mongoose.connect("mongodb+srv://itzbige:<Tmmre123$!TX>@cluster0-gh6wj.mongodb.net/test?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useCreateIndex: true
-}).then(() => {
-    console.log("connected to db!");
-}).catch(err => {
-    console.log("ERROR", err.message);
-});
+mongoose.connect("mongodb://localhost/yelp_camp_v22");
+// mongoose.connect("mongodb+srv://itzbige:<Tmmre123$!TX>@cluster0-gh6wj.mongodb.net/test?retryWrites=true&w=majority", {
+//     useNewUrlParser: true,
+//     useCreateIndex: true
+// }).then(() => {
+//     console.log("connected to db!");
+// }).catch(err => {
+//     console.log("ERROR", err.message);
+// });
 
 // app config
 app.use(bodyParser.urlencoded({extended: true}));
