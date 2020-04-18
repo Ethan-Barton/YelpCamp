28eb8138b1ac3afeba5397caeaffd9632da5815c
Ethan Barton <barton.ethan@gmail>
2020-04-18 13:11:54 -0500

    change port

--- app.js
+++ app.js
@@ -86,6 +86,6 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds/:id/reviews", reviewRoutes);
 
 // Server Config
-app.listen(3000, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
 });
