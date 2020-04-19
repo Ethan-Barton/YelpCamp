ebcf8026f087af9fd4e24907bdf073317553f9d6
Ethan Barton <barton.ethan@gmail>
2020-04-19 07:20:04 -0500

    change DB pass to env

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ mongoose.set("useCreateIndex", true);
 mongoose.set("useFindAndModify", false);
 mongoose.set("useUnifiedTopology", true);
 // mongoose.connect("mongodb://localhost/yelp_camp_v22");
-mongoose.connect("mongodb+srv://itzbige:Tmmre123%24%21TX@cluster0-gh6wj.mongodb.net/test?retryWrites=true&w=majority").then(() => {
+mongoose.connect("mongodb+srv://itzbige:"+ process.env.DBUSERPW +"@cluster0-gh6wj.mongodb.net/test?retryWrites=true&w=majority").then(() => {
     console.log("connected to db!");
 }).catch(err => {
     console.log("ERROR", err.message);
